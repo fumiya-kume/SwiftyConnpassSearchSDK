9e0f1c668b4d168000e5b86c42b957dc3b5a985f
Fumiya Kume <fumiya.kume@hotmail.com>
2018-08-26 11:47:24 +0900

    Initial Commit

---
+++ SwiftyConnpassSearchSDK/SwiftyConnpassSearchSDK.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyConnpassSearchSDK.h
+//  SwiftyConnpassSearchSDK
+//
+//  Created by 久米史也 on 2018/08/26.
+//  Copyright © 2018年 久米史也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyConnpassSearchSDK.
+FOUNDATION_EXPORT double SwiftyConnpassSearchSDKVersionNumber;
+
+//! Project version string for SwiftyConnpassSearchSDK.
+FOUNDATION_EXPORT const unsigned char SwiftyConnpassSearchSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyConnpassSearchSDK/PublicHeader.h>
+
+
